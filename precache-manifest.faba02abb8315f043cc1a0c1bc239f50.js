dbdfe57e2ab6895ce4ecab33d374d3cf9b07dfc7
Amit Gupta <akaamitgupta@gmail.com>
2019-05-30 15:34:07 +0530

    Initial Setup

---
+++ precache-manifest.faba02abb8315f043cc1a0c1bc239f50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f8ef107a28feee86f0d0",
+    "url": "/js/chunk-vendors.6f0e9ad5.js"
+  },
+  {
+    "revision": "18bb649097e0c698cec7",
+    "url": "/js/app.c85fa864.js"
+  },
+  {
+    "revision": "9745b9b716496cd3d833",
+    "url": "/js/about.58c14f04.js"
+  },
+  {
+    "revision": "5ccada1290eea8a223be355fe61a4c14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "18bb649097e0c698cec7",
+    "url": "/css/app.4383fdc6.css"
+  }
+];
